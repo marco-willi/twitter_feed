b431b50b6bb981b2c2d0a4ccf7f6f0e20af7cea7
Marco Willi <will5448@umn.edu>
2017-09-27 00:22:26 -0500

    mongo db listener

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM ubuntu:16.04
 RUN apt-get update && apt-get install -y \
     python3-pip
-RUN pip install tweepy pymongo
+RUN pip3 install --upgrade pip
+RUN pip3 install tweepy pymongo
 RUN mkdir /data
 WORKDIR /code
 CMD ["python3", "main.py"]
--- code/main.py
+++ code/main.py
@@ -3,24 +3,37 @@ from __future__ import absolute_import, print_function
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-
 from credentials import consumer_key, consumer_secret,\
                         access_token, access_token_secret
+from pymongo import MongoClient
+import json
 
 
-class StdOutListener(StreamListener):
+class MongoDBListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
+    This is a mongo db listener, that extracts tweets as json and saves them
+    in the mongo db collection
     """
+    def __init__(self, collection):
+        self.collection = collection
+
     def on_data(self, data):
-        print(data)
-        return True
+        json_dict = json.loads(data)
+        self.collection.insert_one(json_dict)
+        print("Added tweet to db..")
 
     def on_error(self, status):
         print(status)
 
+
 if __name__ == '__main__':
-    l = StdOutListener()
+
+    # Database
+    client = MongoClient('localhost', 27017)
+    db = client['tweets']
+    raw_tweets_collection = db['raw_tweets']
+
+    l = MongoDBListener(raw_tweets_collection)
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
