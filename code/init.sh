e5a8c12b862c9804bc37cfebbe37150c16937f8f
Marco Willi <will5448@umn.edu>
2017-09-27 02:07:31 -0500

    mongo env

--- code/init.sh
+++ code/init.sh
@@ -31,3 +31,6 @@ sudo docker-compose up --build
 # run
 # cd /code
 # python main.py
+
+# sudo docker exec -it twitterfeed_mongodb_1 mongo
+# sudo docker run -d -p 27017:27017 -v /db:/data/db --name mongodb mongodb:latest
